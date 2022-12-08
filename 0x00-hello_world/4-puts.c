202ff03bbb255a658a031d919d8851b0e12bc39b
Bariduanen Uweh <uwehbariduanen@gmail.com>
2022-12-08 14:50:40 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-/**
- * Main - Entry Point
- *
- * Return Always 0 (Success)
- */
+	/*Main: Entry Point */
 int main(void)
 {
+	/*Return: Always 0 (Success)*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
