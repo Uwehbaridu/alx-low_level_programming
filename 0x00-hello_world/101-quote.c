e40e6ca39384b5dfa7eae6523ae9da7578c62dee
Bariduanen Uweh <uwehbariduanen@gmail.com>
2022-12-10 16:26:00 -0800

    Task 8 done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: The last project on hello world
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	return (1);
+}
