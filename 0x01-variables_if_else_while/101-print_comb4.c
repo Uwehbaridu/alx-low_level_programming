7ed855d5bf5e58ed1be9e430ddfc7f3eebefded3
Bariduanen Uweh <uwehbariduanen@gmail.com>
2022-12-11 13:17:29 -0800

    Task done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,21 +21,22 @@ int main(void)
 				{
 					if (j > n)
 					{
-						putchar(i);
-						putchar(n);
-						putchar(j);
-						if (i == 55 && n == 56 && j == 57)
-						{
-							break;
-						}
+					putchar(i);
+					putchar(n);
+					putchar(j);
+					if (i == 55 && n == 56 && j == 57)
+					{
+						break;
+					}
 
-						putchar(',');
-						putchar(' ');
+					putchar(',');
+					putchar(' ');
 					}
 				}
 			}
 		}
 	}
+
 	putchar ('\n');
 	return (0);
 }
