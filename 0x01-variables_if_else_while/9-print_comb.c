524b465962f1e9d88b364f5422b86d7e13a2f606
Bariduanen Uweh <uwehbariduanen@gmail.com>
2022-12-11 01:44:20 -0800

    prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 15; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
-			if (num < 15)
+			if (num < 9)
 			{
 				putchar(',');
 				putchar(' ');
