f2f854411f4b14ccb3ea8a4a9285a0c20afb8969
Bariduanen Uweh <uwehbariduanen@gmail.com>
2022-12-11 12:47:06 -0800

    Task 11 completed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-	int ones = '0'
-	int tens = '0'
+	int ones = '0';
+	int tens = '0';
 
-	for (tense = '0'; tens <= '9'; tense++)/*prints tens digits*/
+	for (tens = '0'; tens <= '9'; tens++) /*prints tens digits*/
 	{
-		for (ones = '0'; ones <= '9'; ones++)/*print tens digits*/
+		for (ones = '0'; ones <= '9'; ones++) /*print tens digits*/
 		{
-			if (!((ones == tense) || (tens > ones)))/*eleminate repeatition*/
+			if (!((ones == tens) || (tens > ones))) /*eleminate repeatition*/
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/* Add comma and space*/
+				if (!(ones == '9' && tens == '8')) /* Add comma and space*/
 				{
 
 					putchar(',');
