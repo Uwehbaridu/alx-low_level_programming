feffde17fd025a6b1e02a787e9e024c4630601e1
Bariduanen Uweh <uwehbariduanen@gmail.com>
2022-12-13 13:48:31 -0800

    update on task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function to print lower case letters
+ *
+ * Return: Always 0
+ */
+
+/**
+ * Fumction declaration
+
+ */
+
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+
+}
