1ef82b0f7618998d0d0f21a7fcb44960049cd41c
Bariduanen Uweh <uwehbariduanen@gmail.com>
2022-12-15 00:40:18 -0800

    Last task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib2_half2, fib2_half1, fib2_half2;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
 	{
-		sum - fib1 + fib2;
+		sum = fib1 + fib2;
 		printf("%lu, ", sum);
 
 		fib1 = fib2;
@@ -45,7 +45,7 @@ int main(void)
 		fib2_half2 = half1;
 		fib2_half2 = half2;
 	}
-	printf("\n"0);
+	printf("\n");
 	return (0);
 }
 
