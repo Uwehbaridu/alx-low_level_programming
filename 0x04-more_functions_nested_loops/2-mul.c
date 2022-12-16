396fe9d9f40d2fa58cbf8520a6eb62a31dcc7c67
Bariduanen Uweh <uwehbariduanen@gmail.com>
2022-12-16 15:28:59 -0800

    task 0 to 10 done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two numbers
+ * @a: first numnber
+ * @b: second number
+ * Return: returns result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
