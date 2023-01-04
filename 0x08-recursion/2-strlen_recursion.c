d9d57e2e53a9f1d51ab13ea9eba068771b2eddc6
Bariduanen Uweh <uwehbariduanen@gmail.com>
2023-01-04 13:29:38 -0800

    Tasks remake

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,18 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * _strlen_recursion - It returns the length of a string.
- * @s: pointer block of memory to fill
+ * factorial - gets factorial of n
+ * @n: integer
  *
- * Return: strlen_recursion
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _strlen_recursion(char *s)
+
+int factorial(int n)
 {
-	if (*s == '\0')
-		return (0);
-
-	else
-		return (1 + _strlen_recursion(s + 1));
+	if (n < 0)
+	return (-1);
+	if (n == 0)
+	return (1);
+	return (n * factorial(n - 1));
 }
-
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,33 @@
 #include "main.h"
 
-int is_divisible(int num, int div);
-int is_prime_number(int n);
-
 /**
- * is_divisible - Checks if a number is divisible.
- * @num: The number to be checked.
- * @div: The divisor.
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  *
- * Return: If the number is divisible - 0.
- * If the number is not divisible - 1.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int is_divisible(int num, int div)
+
+int prime2(int a, int b)
 {
-	if (num % div == 0)
-		return (0);
+	if (a == b)
+	return (1);
+	else if (a % b == 0)
+	return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
-	if (div == num / 2)
-		return (1);
-
-	return (is_divisible(num, div + 1));
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	return (0);
+	return (prime2(n, 2));
 }
