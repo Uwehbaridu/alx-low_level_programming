8bf25674c0c1804169d1563964416711dc2f3a11
Bariduanen Uweh <uwehbariduanen@gmail.com>
2023-01-18 15:25:33 -0800

    Task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,8 @@
-#ifndef FUNCTION_POINTER.H
-#define FUNCTION_POINTER.H
-
-#include <stdio.h>
-#include <unistd.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int op_mul(int a, int b);
+
 #endif
